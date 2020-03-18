68cc0ef3b8a3d3117ebfabf58b1da25176e976cf
xMarkSt <markstam@lijbrandt.nl>
2020-03-18 10:46:40 +0100

    Ardulink test
    Werkt niet met Java 11, wel met Java 8!

--- src/main/java/nl/ictm2a1/kbs2/Main.java
+++ src/main/java/nl/ictm2a1/kbs2/Main.java
@@ -1,10 +1,28 @@
 package nl.ictm2a1.kbs2;
 
+import org.ardulink.core.Link;
+import org.ardulink.core.Pin;
+import org.ardulink.core.convenience.Links;
+import org.ardulink.util.URIs;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Under_Koen on 26/02/2020.
  */
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        try {
+            Link link = Links.getLink(URIs.newURI("ardulink://serial-jssc?port=COM3"));
+            Pin.DigitalPin pin = Pin.digitalPin(13);
+            for (boolean power = true; ; power = !power) {
+                System.out.println("Send power: " + power);
+                link.switchDigitalPin(pin, power);
+                TimeUnit.SECONDS.sleep(2);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
