73bfbafd98fcba4358530e6346c2da1641e5ce89
Under_Koen <koenvanstaveren@hotmail.com>
2020-03-24 12:41:26 +0100

    revert commit, wrong branch

--- src/main/java/nl/ictm2a1/kbs2/Main.java
+++ src/main/java/nl/ictm2a1/kbs2/Main.java
@@ -1,37 +1,10 @@
 package nl.ictm2a1.kbs2;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Created by Under_Koen on 26/02/2020.
  */
 public class Main {
-    private static List<Arduino> arduinos;
-    private static long last;
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws InterruptedException {
-        arduinos = Arduino.findUsbArduinos();
-        for (Arduino arduino : arduinos) {
-            if (arduino.open()) {
-                Thread.sleep(1500);
-                arduino.addReadHandler(s -> {
-                    if (s.isEmpty()) return;
-                    System.out.println(s);
-                    try {
-                        int i = Integer.parseInt(s);
-                        if (i > 512) {
-                            arduino.sendEvent('n', 1);
-                            arduino.sendEvent('f', 2);
-                        } else {
-                            arduino.sendEvent('n', 2);
-                            arduino.sendEvent('f', 1);
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
-            }
-        }
     }
 }
