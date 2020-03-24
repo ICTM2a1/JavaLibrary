b74a85ef0a0955024657649a90ef3fc1de115673
xMarkSt <markstam@lijbrandt.nl>
2020-03-24 12:38:46 +0100

    Start nrjavaserial

--- src/main/java/nl/ictm2a1/kbs2/Main.java
+++ src/main/java/nl/ictm2a1/kbs2/Main.java
@@ -1,10 +1,36 @@
 package nl.ictm2a1.kbs2;
 
+import gnu.io.NRSerialPort;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 /**
  * Created by Under_Koen on 26/02/2020.
  */
 public class Main {
     public static void main(String[] args) {
+        for(String s: NRSerialPort.getAvailableSerialPorts()){
+            System.out.println("Availible port: "+s);
+        }
+        String port = "COM3";
+        int baudRate = 9600;
+        NRSerialPort serial = new NRSerialPort(port, baudRate);
+        serial.connect();
 
+        DataInputStream ins = new DataInputStream(serial.getInputStream());
+        DataOutputStream outs = new DataOutputStream(serial.getOutputStream());
+
+        try {
+            int b = ins.read();
+            System.out.println(b);
+            outs.write(b);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        serial.disconnect();
     }
 }
