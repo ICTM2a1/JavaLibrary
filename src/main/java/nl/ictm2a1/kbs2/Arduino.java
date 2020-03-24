e3e24054d1a8c6a1ba37aa072cf099093329a92c
xMarkSt <markstam@lijbrandt.nl>
2020-03-24 13:25:31 +0100

    nrjavaserial

---
+++ src/main/java/nl/ictm2a1/kbs2/Arduino.java
@@ -0,0 +1,127 @@
+package nl.ictm2a1.kbs2;
+
+import gnu.io.NRSerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TooManyListenersException;
+import java.util.function.Consumer;
+
+/**
+ * Created by Under_Koen on 18/03/2020.
+ */
+public class Arduino implements Closeable {
+    public static List<Arduino> findUsbArduinos() {
+        List<Arduino> arduinos = new ArrayList<>();
+        for (String s : NRSerialPort.getAvailableSerialPorts()) {
+            NRSerialPort port = new NRSerialPort(s, 9600);
+            System.out.println(s);
+            arduinos.add(new Arduino(port));
+        }
+
+        return arduinos;
+    }
+
+    private NRSerialPort port;
+    private int baudRate;
+    private List<Consumer<String>> readHandlers;
+    private SerialReader serialReader;
+    private PrintWriter printWriter;
+
+    public Arduino(NRSerialPort port) {
+        this.port = port;
+        this.readHandlers = new ArrayList<>();
+        setBaudRate(9600);
+    }
+
+    public void setBaudRate(int baudRate) {
+        port.setBaud(baudRate);
+        this.baudRate = baudRate;
+    }
+
+    /**
+     * @return the baudrate of the port.<br><b>Default:</b> 9600
+     */
+    public int getBaudRate() {
+        return baudRate;
+    }
+
+    public void sendEvent(char t, int v) {
+        write(t);
+        write((char) v);
+        flush();
+    }
+
+    public void write(String s) {
+        if (printWriter == null) printWriter = new PrintWriter(port.getOutputStream());
+        printWriter.write(s);
+    }
+
+    public void write(char c) {
+        if (printWriter == null) printWriter = new PrintWriter(port.getOutputStream());
+        printWriter.write(c);
+    }
+
+    public void writeln(String s) {
+        write(s + "\n");
+        printWriter.flush();
+    }
+
+    public void flush() {
+        printWriter.flush();
+    }
+
+    public boolean open() {
+        return port.connect();
+    }
+
+    @Override
+    public void close() {
+        port.disconnect();
+    }
+
+    public void addReadHandler(Consumer<String> readHandler) throws TooManyListenersException {
+        readHandlers.add(readHandler);
+        if (serialReader == null) {
+            serialReader = new SerialReader((s) -> {
+                String s2 = s.replaceAll("[\r\n\b\f]", "");
+                readHandlers.forEach(c -> c.accept(s2));
+            });
+            port.addEventListener(serialReader);
+        }
+    }
+
+    private static class SerialReader implements SerialPortEventListener {
+        private InputStream in;
+        private byte[] buffer = new byte[1024];
+
+        public SerialReader(InputStream in) {
+            this.in = in;
+        }
+
+        public void serialEvent(SerialPortEvent arg0) {
+            int data;
+
+            try {
+                int len = 0;
+                while ((data = in.read()) > -1) {
+                    if (data == '\n') {
+                        break;
+                    }
+                    buffer[len++] = (byte) data;
+                }
+                System.out.print(new String(buffer, 0, len));
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+
+    }
+}
--- src/main/java/nl/ictm2a1/kbs2/Main.java
+++ src/main/java/nl/ictm2a1/kbs2/Main.java
@@ -5,32 +5,35 @@ import gnu.io.NRSerialPort;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.TooManyListenersException;
 
 /**
  * Created by Under_Koen on 26/02/2020.
  */
 public class Main {
     public static void main(String[] args) {
-        for(String s: NRSerialPort.getAvailableSerialPorts()){
-            System.out.println("Availible port: "+s);
+        List<Arduino> arduinos = Arduino.findUsbArduinos();
+        for (Arduino arduino : arduinos) {
+            try {
+                if (arduino.open()) {
+                    Thread.sleep(1500);
+                    arduino.addReadHandler(s -> {
+                        if (s.isEmpty()) return;
+                        System.out.println(s);
+                        int i = Integer.parseInt(s);
+                        if (i > 512) {
+                            arduino.sendEvent('n', 1);
+                            arduino.sendEvent('f', 2);
+                        } else {
+                            arduino.sendEvent('n', 2);
+                            arduino.sendEvent('f', 1);
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        String port = "COM3";
-        int baudRate = 9600;
-        NRSerialPort serial = new NRSerialPort(port, baudRate);
-        serial.connect();
-
-        DataInputStream ins = new DataInputStream(serial.getInputStream());
-        DataOutputStream outs = new DataOutputStream(serial.getOutputStream());
-
-        try {
-            int b = ins.read();
-            System.out.println(b);
-            outs.write(b);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        serial.disconnect();
     }
 }
