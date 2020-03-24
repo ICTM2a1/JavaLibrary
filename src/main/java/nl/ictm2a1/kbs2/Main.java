705167e34eb54d587458cf1cd984d1eddf93dd43
Under_Koen <koenvanstaveren@hotmail.com>
2020-03-24 12:36:11 +0100

    Almost done with jSerialComm

--- src/main/java/nl/ictm2a1/kbs2/Main.java
+++ src/main/java/nl/ictm2a1/kbs2/Main.java
@@ -1,45 +1,36 @@
 package nl.ictm2a1.kbs2;
 
-import com.fazecast.jSerialComm.SerialPort;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 /**
  * Created by Under_Koen on 26/02/2020.
  */
 public class Main {
-    public static void main(String[] args) throws InterruptedException, IOException {
-        SerialPort[] ports = SerialPort.getCommPorts();
-        List<SerialPort> usbs = Arrays.stream(ports)
-                .filter(serialPort -> serialPort.getSystemPortName().contains("cu.usb"))
-                .collect(Collectors.toList());
+    private static List<Arduino> arduinos;
+    private static long last;
 
-        for (SerialPort usb : usbs) {
-            usb.setBaudRate(9600);
-            if (usb.openPort()) {
-                Thread.sleep(100);
-                usb.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
-                InputStream in = usb.getInputStream();
-                Scanner scannerInput = new Scanner(System.in);
-                Scanner scannerArduino = new Scanner(usb.getInputStream());
-                PrintWriter out = new PrintWriter(usb.getOutputStream());
-                while(true) {
-                    if (in.available() > 0) {
-                        System.out.println(scannerArduino.next());
-                    } else if (System.in.available() > 0) {
-                        out.println(scannerInput.next());
-                        out.flush();
+    public static void main(String[] args) throws InterruptedException {
+        arduinos = Arduino.findUsbArduinos();
+        for (Arduino arduino : arduinos) {
+            if (arduino.open()) {
+                Thread.sleep(1500);
+                arduino.addReadHandler(s -> {
+                    if (s.isEmpty()) return;
+                    System.out.println(s);
+                    try {
+                        int i = Integer.parseInt(s);
+                        if (i > 512) {
+                            arduino.sendEvent('n', 1);
+                            arduino.sendEvent('f', 2);
+                        } else {
+                            arduino.sendEvent('n', 2);
+                            arduino.sendEvent('f', 1);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                    Thread.sleep(100);
-                }
-            } else {
-                System.out.println(usb.getSystemPortName() + " INCORRECT");
+                });
             }
         }
     }
